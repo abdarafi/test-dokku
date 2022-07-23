ffe544f7b979d67ab7d9d68b962182a36bb0c3ac
Abda Rafi <abda_rafi@yahoo.com>
2022-07-23 17:45:53 +0700

    finalize CI/CD

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Abraca..What!!")
+		fmt.Fprintf(w, "Abraca..What!!\nCI/CD is working now.")
 	})
 	fmt.Printf("Server running (port=8080), route: http://localhost:8080/\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
